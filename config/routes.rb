b83c6fbcfdb403c51e7a4f96e10c6b16ded76696
ThaiBui <thaibuixuan34@gmail.com>
2020-02-12 21:47:01 +0700

    Finish basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   get 'static_pages/home', to: 'static_pages#home'
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
